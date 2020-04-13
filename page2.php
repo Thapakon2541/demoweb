29661234777ccd941bd0e88c82581d0aeb70d807
anavit2539 <40802718+anavit2539@users.noreply.github.com>
2020-04-13 14:54:10 +0700

    11 Add new file

--- page2.php
+++ page2.php
@@ -4,6 +4,18 @@
 
 <?php
 echo "My second page PHP script!";
+
+$x = 5985;
+var_dump(is_numeric($x));
+
+$x = "5985";
+var_dump(is_numeric($x));
+
+$x = "59.85" + 100;
+var_dump(is_numeric($x));
+
+$x = "Hello";
+var_dump(is_numeric($x));
 ?>
 
 </body>
